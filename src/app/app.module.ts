3dad83afe3bce17fbbe372fb456f55e60c02720c
pedrohenrique57 <pedro.henrique.contato369@gmail.com>
2022-05-29 03:18:34 -0300

    Projeto Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+  ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-aula',
+    appId: '1:746103863636:web:4fdd60478685c22de0f0c1',
+    storageBucket: 'diario-viagens-aula.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBeir1ERehjpR8YylGsHaoaRw7OqH3bSJk',
+    authDomain: 'diario-viagens-aula.firebaseapp.com',
+    messagingSenderId: '746103863636',
+    measurementId: 'G-PVLQYS0Z0M',
+  },
   production: false
 };
 
